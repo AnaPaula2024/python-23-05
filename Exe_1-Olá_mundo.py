fd021057f9c81e5aa00c27757ffa584d2d17aa81
AnaPaula2024 <162187461+AnaPaula2024@users.noreply.github.com>
2024-05-23 22:41:10 -0300

    Add files via upload

---
+++ Exe_1-Olá_mundo.py
@@ -0,0 +1,28 @@
+#===========================================================================
+#Autor: Júlio Miranda
+#Data: 23/05/2024
+#Versão: 1.0
+#Descrição: Algoritmo que recebe o nome do usuário
+#           e exibi uma frase de saudação concatenada com
+#           a entrada do usuário.
+#===========================================================================
+#Observações:
+#           a) para iniciar um comentário utiliza-se '#'
+#           b) no virtual studio o comando 'crtl' + ';'
+#               para comentar um bloco de código
+#===========================================================================
+#Início
+
+dadoVazio = '' #var atribuida com um valor vazio
+#entrada
+nome = input('Qual seu nome?')
+#processamento
+mensagem = 'Olá, seja bem-vindo!'
+frase = mensagem + nome
+#saída 
+print(frase)
+
+#Fim
+#==========================================================================
+
+
---
+++ Exe_2-Tipos_dados.py
@@ -0,0 +1,25 @@
+#===========================================================================
+#Autor: Júlio Miranda
+#Data: 23/05/2024
+#Versão: 1.0
+#Descrição: Algoritmo que recebe o nome do usuário
+#           e exibi uma frase de saudação concatenada com
+#           a entrada do usuário.
+#===========================================================================
+#Observações:
+#           a) para iniciar um comentário utiliza-se '#'
+#           b) no virtual studio o comando 'crtl' + ';'
+#               para comentar um bloco de código
+#===========================================================================
+#Início
+#String -> palavras 'utiliza-se aspas simples ou duplas'
+computador = 'DELL'
+print('A variável computador é do tipo: ', type(computador)) 
+#Integer(Inteiro) -> números inteiros exemplo: 1; 2; 3; 4; 5; ...; 1000
+idade = 53 
+print('A variável idade é do tipo: ', type(idade))
+#Float(Flutuante) -> números fracionados exemplo: 1.3 e 2.5
+altura = 1.8
+print('A variável  altura é do tipo: ', type(altura))
+
+#Fim
