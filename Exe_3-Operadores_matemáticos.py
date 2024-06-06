3befffa6b494c02f048859c1e2d72d486a17a574
Ana Paula <162187461+AnaPaula2024@users.noreply.github.com>
2024-06-06 19:04:56 -0300

    Add files via upload

---
+++ Exe_3-Operadores_matemáticos.py
@@ -0,0 +1,30 @@
+#================================================================
+#Autor: Júlio Miranda
+#Data: 24/05/2024
+#Versão: 1.0
+#Descrição: Trabalhar com as quatro operações matemáticas básicas
+#       + (soma)
+#       - (menos)
+#       * (multiplicação)
+#       / (divisão)
+#================================================================
+#Início
+#Variáveis
+#Substituir para entrada do usuário (input)
+numeroUm = 10
+numeroDois = 5
+resultado = 0 #variável para receber o resultado da operação
+#Soma 
+resultado = numeroUm + numeroDois
+print(numeroUm, '+', numeroDois, '=', resultado)
+#Subtração
+resultado = numeroUm - numeroDois
+print(numeroUm, '-', numeroDois, '=', resultado)
+#Multiplicação
+resultado = numeroUm * numeroDois
+print(numeroUm, '*', numeroDois, '=', resultado)
+#Divisão
+resultado = numeroUm / numeroDois
+print(numeroUm, '/', numeroDois, '=', resultado)
+#Fim
+#===============================================================
