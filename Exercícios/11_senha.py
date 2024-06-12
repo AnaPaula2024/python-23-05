4e79a4ce751800a3b8716a51cd8768b41d93af72
Ana Paula <162187461+AnaPaula2024@users.noreply.github.com>
2024-06-11 22:40:06 -0300

    Add files via upload

--- Exercícios/11_senha.py
+++ Exercícios/11_senha.py
@@ -8,14 +8,33 @@ Descrição:  Faça um programa que solicite para o usuário
              cadastrada (senai115), caso passe desse limite uma mensagem de erro deve aparecer.
 '''
 
-senha = 'senac115'
-tentativas = 3
+# senhaCorreta = 'senac115'
+# limiteDetentativas = 3
+#     while limiteDetentativas > 0:
+#         senha = input("Digite sua senha de acesso: ")
+#         if senha == senhaCorreta:
+#             print("Senha correta! Acesso permitido.")
+#             break
+#         else:
+#             limiteDetentativas -= 1
+#             if limiteDetentativas > 0:
+#                 print("Senha incorreta! Você tem {limiteDetentativas} tentativas restantes.")
+#             else:
+#                 print("Limite de tentativas excedido. Acesso negado.")
+#                 break
+#=============================================================================
+senha = '' #var para receber a senha do usuario
+senhaPadrao = 'senai115' #senha padrão do sistema
+tentativas = 3 #numero de tentativas de acesso ao sistema
 while True:
-    senha = input('Insira sua senha de acesso: ')
-    if(senha == 'senac115')
-        print('Seja bem-vindo')
-    while senha < tentativa:
-        tentativa = tentativa + 1
-    print('Senha incorreta')
-
-
+        senha = input('Digite a sua senha: ') #senai115 => numeros com letras então sem casting
+        if senha == senhaPadrao:
+            print('Acesso liberado!')
+            break #quebra o loop while, ou seja finaliza o programa
+        else:
+            tentativas = tentativas - 1 #tentativas -= 1(mesma coisa)
+            print('Você só possui mais', tentativas,'tentativas')
+        if(tentativas <= 0):
+            print('Sistema Bloqueado')
+            break   
+#=============================================================================
