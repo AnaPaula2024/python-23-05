db7931e4b8aa4be98921e66c5d8485424a906153
Ana Paula <162187461+AnaPaula2024@users.noreply.github.com>
2024-06-13 22:34:15 -0300

    Add files via upload

---
+++ Exe_7_for.py
@@ -0,0 +1,21 @@
+'''
+Autor: Júlio Miranda
+Data: 13/06/2024
+Versão: 1.0
+Descrição: Estudo da estrutura de repetição "for"
+
+'''
+
+#========================================================
+indice_um = 1
+while indice_um <= 6:
+    print('indice:', indice_um)
+    indice_um = indice_um + 1 #indice += 1
+#========================================================
+
+
+for indice_dois in range(6):
+    print('indice', indice_dois) # 0 -> 1 -> 2 -> ..... até 5
+
+#=========================================================
+
